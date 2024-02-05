d39626a0b626da24ffe4c6e0c3a4e3abd56316ce
cubanFree <alvarofuentesbarrera@gmail.com>
2024-02-05 07:55:04 -0800

    fix etc

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,4 @@
-import { Inter } from 'next/font/google'
+import { Content, Inter } from 'next/font/google'
 import './globals.css'
 import { VscServerProcess } from "react-icons/vsc";
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
@@ -17,6 +17,7 @@ export default async function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" type='image/png' href="/logo.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
       </head>
       <body className={inter.className + " text-gray-300 w-full h-screen overflow-x-hidden relative"}>
           <Alert className="bg-yellow-600 flex justify-center gap-2 font-bold border-0 p-1 absolute top-0 z-50 sm:gap-4">
